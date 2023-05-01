698ac55281ff65ef577e71dc1a93dd86ebcb230a
Маргарита <113328619+MaargoGysarova@users.noreply.github.com>
2023-05-01 21:53:45 +0400

    Write symmetric class

---
+++ symmetrical_encrpt.py
@@ -0,0 +1,62 @@
+import os
+from cryptography.hazmat.primitives import padding as sym_padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import logging
+
+settings = {
+    'initial_file': 'path/to/inital/file.txt',
+    'encrypted_file': 'path/to/encrypted/file.txt',
+    'decrypted_file': 'path/to/decrypted/file.txt',
+    'symmetric_key': 'path/to/symmetric/key.txt',
+    'public_key': 'path/to/public/key.pem',
+    'secret_key': 'path/to/secret/key.pem',
+}
+
+
+class SymmetricalEncryption:
+    def __init__(self, size: int, decrypt_file: str, encrypt_file: str,
+                 symmetric_key_file: str = settings["symmetric_key"]) -> None:
+        self.size = size
+        self.sym_key_file = symmetric_key_file
+        self.decrypt_file = decrypt_file
+        self.encrypt_file = encrypt_file
+
+    def generation_symmetric_key(self):
+        return os.urandom(self.size)
+
+    def serialization_symmetric_key(self, key):
+        try:
+            with open(self.sym_key_file, 'wb') as key_out:
+                key_out.write(key)
+        except:
+            logging.error(f"error in file")
+
+    # десериализация симметричного ключа
+    def deserialization_symmetric_key(self):
+        try:
+            with open(self.sym_key_file, 'rb') as key_in:
+                key = key_in.read()
+        except:
+            logging.error(f"error in file")
+        return key
+
+    # шифрование и паддинг текста симметричным алгоритмом
+    def encrypt_simm(self, key, data):
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.ChaCha20(self.deserialization_symmetric_key()), modes.CBC(iv))
+        encryptor = cipher.encryptor()
+        padder = sym_padding.PKCS7(128).padder()
+        padded_data = padder.update(data) + padder.finalize()
+        ct = encryptor.update(padded_data) + encryptor.finalize()
+        return iv + ct
+
+    # дешифрование и депаддинг текста симметричным алгоритмом
+    def decrypt_simm(self, key, data):
+        iv = data[:16]
+        ct = data[16:]
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+        decryptor = cipher.decryptor()
+        padded_data = decryptor.update(ct) + decryptor.finalize()
+        unpadder = sym_padding.PKCS7(128).unpadder()
+        data = unpadder.update(padded_data) + unpadder.finalize()
+        return data
